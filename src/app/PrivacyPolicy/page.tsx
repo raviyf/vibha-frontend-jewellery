0075b5d3b7a46ce2771d679bf7eef8f9caee3310
Ravi <ravindran@yellowflash.ai>
2024-03-23 12:52:54 +0530

    formated privacy policy

--- src/app/PrivacyPolicy/page.tsx
+++ src/app/PrivacyPolicy/page.tsx
@@ -1,8 +1,9 @@
 export default function Page() {
   return (
     <div className="lg:p-16 p-5 xl:p-28 border-b-2">
-      Privacy Policy: This Privacy Policy describes Our policies and procedures
-      on the collection, use and disclosure of Your information when You use the
+      <h1> Privacy Policy:</h1>
+      <br /> This Privacy Policy describes Our policies and procedures on the
+      collection, use and disclosure of Your information when You use the
       Service and tells You about Your privacy rights and how the law protects
       You. We use Your Personal data to provide and improve the Service. By
       using the Service, You agree to the collection and use of information in
@@ -15,18 +16,18 @@ export default function Page() {
       Privacy Policy: Account means a unique account created for You to access
       our Service or parts of our Service. Affiliate means an entity that
       controls, is controlled by or is under common control with a party, where
-      "control" means ownership of 50% or more of the shares, equity interest or
+      control means ownership of 50% or more of the shares, equity interest or
       other securities entitled to vote for election of directors or other
-      managing authority. Company (referred to as either "the Company", "We",
-      "Us" or "Our" in this Agreement) refers to Casa Vibha. Cookies are small
-      files that are placed on Your computer, mobile device or any other device
-      by a website, containing the details of Your browsing history on that
-      website among its many uses. Country refers to: Tamil Nadu, India Device
-      means any device that can access the Service such as a computer, a
-      cellphone or a digital tablet. Personal Data is any information that
-      relates to an identified or identifiable individual. Service refers to the
-      Website. Service Provider means any natural or legal person who processes
-      the data on behalf of the Company. It refers to third-party companies or
+      managing authority. Company (referred to as either the Company , We , Us
+      or Our in this Agreement) refers to Casa Vibha. Cookies are small files
+      that are placed on Your computer, mobile device or any other device by a
+      website, containing the details of Your browsing history on that website
+      among its many uses. Country refers to: Tamil Nadu, India Device means any
+      device that can access the Service such as a computer, a cellphone or a
+      digital tablet. Personal Data is any information that relates to an
+      identified or identifiable individual. Service refers to the Website.
+      Service Provider means any natural or legal person who processes the data
+      on behalf of the Company. It refers to third-party companies or
       individuals employed by the Company to facilitate the Service, to provide
       the Service on behalf of the Company, to perform services related to the
       Service or to assist the Company in analyzing how the Service is used.
@@ -38,35 +39,38 @@ export default function Page() {
       Vibha, accessible from www.casavibha.com You means the individual
       accessing or using the Service, or the company, or other legal entity on
       behalf of which such individual is accessing or using the Service, as
-      applicable. Collecting and Using Your Personal Data Types of Data
-      Collected Personal Data While using Our Service, We may ask You to provide
-      Us with certain personally identifiable information that can be used to
-      contact or identify You. Personally identifiable information may include,
-      but is not limited to: Email address First name and last name Phone number
-      Address, State, Province, ZIP/Postal code, City Usage Data Usage Data
-      Usage Data is collected automatically when using the Service. Usage Data
-      may include information such as Your Device's Internet Protocol address
-      (e.g. IP address), browser type, browser version, the pages of our Service
-      that You visit, the time and date of Your visit, the time spent on those
-      pages, unique device identifiers and other diagnostic data. When You
-      access the Service by or through a mobile device, We may collect certain
-      information automatically, including, but not limited to, the type of
-      mobile device You use, Your mobile device unique ID, the IP address of
-      Your mobile device, Your mobile operating system, the type of mobile
-      Internet browser You use, unique device identifiers and other diagnostic
-      data. We may also collect information that Your browser sends whenever You
-      visit our Service or when You access the Service by or through a mobile
-      device. Information from Third-Party Social Media Services The Company
-      allows You to create an account and log in to use the Service through the
-      following Third-party Social Media Services: Google Facebook Instagram
-      Twitter LinkedIn If You decide to register through or otherwise grant us
-      access to a Third-Party Social Media Service, We may collect Personal data
-      that is already associated with Your Third-Party Social Media Service's
-      account, such as Your name, Your email address, Your activities or Your
-      contact list associated with that account. You may also have the option of
-      sharing additional information with the Company through Your Third-Party
-      Social Media Service's account. If You choose to provide such information
-      and Personal Data, during registration or otherwise, You are giving the
+      applicable.
+      <br />
+      Collecting and Using Your Personal Data Types of Data Collected Personal
+      Data While using Our Service, We may ask You to provide Us with certain
+      personally identifiable information that can be used to contact or
+      identify You. Personally identifiable information may include, but is not
+      limited to: Email address First name and last name Phone number Address,
+      State, Province, ZIP/Postal code, City Usage Data Usage Data Usage Data is
+      collected automatically when using the Service. Usage Data may include
+      information such as Your Device s Internet Protocol address (e.g. IP
+      address), browser type, browser version, the pages of our Service that You
+      visit, the time and date of Your visit, the time spent on those pages,
+      unique device identifiers and other diagnostic data. When You access the
+      Service by or through a mobile device, We may collect certain information
+      automatically, including, but not limited to, the type of mobile device
+      You use, Your mobile device unique ID, the IP address of Your mobile
+      device, Your mobile operating system, the type of mobile Internet browser
+      You use, unique device identifiers and other diagnostic data. We may also
+      collect information that Your browser sends whenever You visit our Service
+      or when You access the Service by or through a mobile device.
+      <br />
+      Information from Third-Party Social Media Services The Company allows You
+      to create an account and log in to use the Service through the following
+      Third-party Social Media Services: Google Facebook Instagram Twitter
+      LinkedIn If You decide to register through or otherwise grant us access to
+      a Third-Party Social Media Service, We may collect Personal data that is
+      already associated with Your Third-Party Social Media Service s account,
+      such as Your name, Your email address, Your activities or Your contact
+      list associated with that account. You may also have the option of sharing
+      additional information with the Company through Your Third-Party Social
+      Media Service s account. If You choose to provide such information and
+      Personal Data, during registration or otherwise, You are giving the
       Company permission to use, share, and store it in a manner consistent with
       this Privacy Policy. Tracking Technologies and Cookies We use Cookies and
       similar tracking technologies to track the activity on Our Service and
@@ -84,38 +88,39 @@ export default function Page() {
       to count users who have visited those pages or opened an email and for
       other related website statistics (for example, recording the popularity of
       a certain section and verifying system and server integrity). Cookies can
-      be "Persistent" or "Session" Cookies. Persistent Cookies remain on Your
+      be Persistent or Session Cookies. Persistent Cookies remain on Your
       personal computer or mobile device when You go offline, while Session
       Cookies are deleted as soon as You close Your web browser. Learn more
-      about cookies on the Privacy Policies website article. We use both Session
-      and Persistent Cookies for the purposes set out below: Necessary /
-      Essential Cookies Type: Session Cookies Administered by: Us Purpose: These
-      Cookies are essential to provide You with services available through the
-      Website and to enable You to use some of its features. They help to
-      authenticate users and prevent fraudulent use of user accounts. Without
-      these Cookies, the services that You have asked for cannot be provided,
-      and We only use these Cookies to provide You with those services. Cookies
-      Policy / Notice Acceptance Cookies Type: Persistent Cookies Administered
-      by: Us Purpose: These Cookies identify if users have accepted the use of
-      cookies on the Website. Functionality Cookies Type: Persistent Cookies
-      Administered by: Us Purpose: These Cookies allow us to remember choices
-      You make when You use the Website, such as remembering your login details
-      or language preference. The purpose of these Cookies is to provide You
-      with a more personal experience and to avoid You having to re-enter your
-      preferences every time You use the Website. For more information about the
-      cookies we use and your choices regarding cookies, please visit our
-      Cookies Policy or the Cookies section of our Privacy Policy. Use of Your
-      Personal Data The Company may use Personal Data for the following
-      purposes: To provide and maintain our Service, including to monitor the
-      usage of our Service. To manage Your Account: to manage Your registration
-      as a user of the Service. The Personal Data You provide can give You
-      access to different functionalities of the Service that are available to
-      You as a registered user. For the performance of a contract: the
-      development, compliance and undertaking of the purchase contract for the
-      products, items or services You have purchased or of any other contract
-      with Us through the Service. To contact You: To contact You by email,
-      telephone calls, SMS, or other equivalent forms of electronic
-      communication, such as a mobile application's push notifications regarding
+      about cookies on the Privacy Policies website article. <br />
+      We use both Session and Persistent Cookies for the purposes set out below:
+      Necessary / Essential Cookies Type: Session Cookies Administered by: Us
+      Purpose: These Cookies are essential to provide You with services
+      available through the Website and to enable You to use some of its
+      features. They help to authenticate users and prevent fraudulent use of
+      user accounts. Without these Cookies, the services that You have asked for
+      cannot be provided, and We only use these Cookies to provide You with
+      those services. Cookies Policy / Notice Acceptance Cookies Type:
+      Persistent Cookies Administered by: Us Purpose: These Cookies identify if
+      users have accepted the use of cookies on the Website. Functionality
+      Cookies Type: Persistent Cookies Administered by: Us Purpose: These
+      Cookies allow us to remember choices You make when You use the Website,
+      such as remembering your login details or language preference. The purpose
+      of these Cookies is to provide You with a more personal experience and to
+      avoid You having to re-enter your preferences every time You use the
+      Website. <br />
+      For more information about the cookies we use and your choices regarding
+      cookies, please visit our Cookies Policy or the Cookies section of our
+      Privacy Policy. Use of Your Personal Data The Company may use Personal
+      Data for the following purposes: To provide and maintain our Service,
+      including to monitor the usage of our Service. To manage Your Account: to
+      manage Your registration as a user of the Service. The Personal Data You
+      provide can give You access to different functionalities of the Service
+      that are available to You as a registered user. For the performance of a
+      contract: the development, compliance and undertaking of the purchase
+      contract for the products, items or services You have purchased or of any
+      other contract with Us through the Service. To contact You: To contact You
+      by email, telephone calls, SMS, or other equivalent forms of electronic
+      communication, such as a mobile application s push notifications regarding
       updates or informative communications related to the functionalities,
       products or contracted services, including the security updates, when
       necessary or reasonable for their implementation. To provide You with
@@ -164,7 +169,7 @@ export default function Page() {
       data is used to strengthen the security or to improve the functionality of
       Our Service, or We are legally obligated to retain this data for longer
       time periods. Transfer of Your Personal Data Your information, including
-      Personal Data, is processed at the Company's operating offices and in any
+      Personal Data, is processed at the Company s operating offices and in any
       other places where the parties involved in the processing are located. It
       means that this information may be transferred to — and maintained on —
       computers located outside of Your state, province, country or other
@@ -202,7 +207,7 @@ export default function Page() {
       Personal Data is important to Us, but remember that no method of
       transmission over the Internet, or method of electronic storage is 100%
       secure. While We strive to use commercially acceptable means to protect
-      Your Personal Data, We cannot guarantee its absolute security. Children's
+      Your Personal Data, We cannot guarantee its absolute security. Children s
       Privacy Our Service does not address anyone under the age of 13. We do not
       knowingly collect personally identifiable information from anyone under
       the age of 13. If You are a parent or guardian and You are aware that Your
@@ -211,10 +216,10 @@ export default function Page() {
       without verification of parental consent, We take steps to remove that
       information from Our servers. If We need to rely on consent as a legal
       basis for processing Your information and Your country requires consent
-      from a parent, We may require Your parent's consent before We collect and
+      from a parent, We may require Your parent s consent before We collect and
       use that information. Links to Other Websites Our Service may contain
       links to other websites that are not operated by Us. If You click on a
-      third party link, You will be directed to that third party's site. We
+      third party link, You will be directed to that third party s site. We
       strongly advise You to review the Privacy Policy of every site You visit.
       We have no control over and assume no responsibility for the content,
       privacy policies or practices of any third party sites or services.
@@ -222,9 +227,10 @@ export default function Page() {
       to time. We will notify You of any changes by posting the new Privacy
       Policy on this page. We will let You know via email and/or a prominent
       notice on Our Service, prior to the change becoming effective and update
-      the "Last updated" date at the top of this Privacy Policy. You are advised
-      to review this Privacy Policy periodically for any changes. Changes to
-      this Privacy Policy are effective when they are posted on this page.
+      the Last updated date at the top of this Privacy Policy. You are advised
+      to review this Privacy Policy periodically for any changes. <br />
+      Changes to this Privacy Policy are effective when they are posted on this
+      page. <br />
       Contact Us If you have any questions about this Privacy Policy, You can
       contact us: By email: help@casavibha.com
     </div>
